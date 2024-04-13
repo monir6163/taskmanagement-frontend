640f4d98e86d798f0c3ecb91ba265b73cf61d540
netstermonir <monirhossain6163@gmail.com>
2024-04-13 12:34:47 +0600

    title dynamic

--- src/app/(auth)/(signin)/page.jsx
+++ src/app/(auth)/(signin)/page.jsx
@@ -1,12 +1,13 @@
 import Login from "@/components/login";
+import PageTitle from "@/components/pageTitle";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-
 export default async function Home() {
   const session = await getServerSession();
   if (session) {
     redirect("/dashboard");
   }
+  PageTitle("Sign In");
   return (
     <main className="flex justify-center items-center h-screen">
       <div className="max-w-md w-full m-auto my-10 space-y-10">
